9c17dc2ea0d8fed3175b32270c8819650c6d4049
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-24 18:07:05 -0300

    Fix: minor fixes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,52 +2,57 @@ import { HeaderContainer, Options } from "../../components/Header/style";
 import { useContext, useState } from "react";
 import logo from "../../images/linkr-logo.png";
 import { AiOutlineDown } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { TokenContext } from "../../context/UserContext";
 import SearchBar from "../SearchBar/SearchBar";
 
 export default function Header() {
-  const [clicked, setClicked] = useState(false);
-  const { token } = useContext(TokenContext);
-  const navigate = useNavigate();
+	const [clicked, setClicked] = useState(false);
+	const { token } = useContext(TokenContext);
+	const navigate = useNavigate();
 
-  function click() {
-    setClicked(!clicked)
-  }
+	function click() {
+		setClicked(!clicked);
+	}
 
-  function logout() {
-    const config = {
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    };
-    axios
-      .delete(`${process.env.REACT_APP_API_URL}/logout`, config)
-      .then(() => {
-        localStorage.removeItem("token");
-        navigate("/");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+	function logout() {
+		const config = {
+			headers: {
+				authorization: `Bearer ${token}`,
+			},
+		};
+		axios
+			.delete(`${process.env.REACT_APP_API_URL}/logout`, config)
+			.then(() => {
+				localStorage.removeItem("token");
+				navigate("/");
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  return (
-    <HeaderContainer>
-      <img src={logo} alt="logo" />
-      <SearchBar />
-      <div onClick={click}>
-        <AiOutlineDown clicked={clicked} />
+	return (
+		<HeaderContainer>
+			<Link to={"/timeline"}>
+				<img
+					src={logo}
+					alt="logo"
+				/>
+			</Link>
+			<SearchBar />
+			<div onClick={click}>
+				<AiOutlineDown clicked={clicked} />
 
-        <img
-          src="https://e7.pngegg.com/pngimages/708/344/png-clipart-dogs-dogs.png"
-          alt="user"
-        />
-      </div>
-      <Options clicked={clicked}>
-        <p onClick={logout}>Logout</p>
-      </Options>
-    </HeaderContainer>
-  );
+				<img
+					src="https://e7.pngegg.com/pngimages/708/344/png-clipart-dogs-dogs.png"
+					alt="user"
+				/>
+			</div>
+			<Options clicked={clicked}>
+				<p onClick={logout}>Logout</p>
+			</Options>
+		</HeaderContainer>
+	);
 }
